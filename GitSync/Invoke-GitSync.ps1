178ad9936bad551258ea95a817db5f32fa02feb7
AchimASR <achim.wieser@appsphere.com>
2017-06-14 12:02:39 +0200

    Refactoring Invoke-GitSync.ps1

--- GitSync/Invoke-GitSync.ps1
+++ GitSync/Invoke-GitSync.ps1
@@ -8,8 +8,11 @@ Clone a Git repository to ScriptRunner Library or pull updates to a local reposi
 .PARAMETER GitRepoUrl
 URL of the git repository. e.g. 'https://github.com/PowerShell/PowerShell.git'
 
-.PARAMETER GitUser
+.PARAMETER GitUserCredential
 Credential of a git user, who is authorized to access the given git repo.
+
+.PARAMETER GitUserName
+UserName of a git user, who is authorized to access the given git repo.
 
 .PARAMETER SRLibraryPath
 Path to the ScriptRunner Library Path. Default: 'C:\ProgramData\AppSphere\ScriptMgr'
@@ -29,57 +32,76 @@ https://github.com/Microsoft/Git-Credential-Manager-for-Windows
 
 #>
 
-[CmdletBinding()]
+[CmdletBinding(DefaultParameterSetName='Credential')]
 param(
     [Parameter(Mandatory = $true)]
     [string]$GitRepoUrl,
-    [Parameter(Mandatory = $true)]
-    [pscredential]$GitUser,
-    [string]$SRLibraryPath = 'C:\ProgramData\AppSphere\ScriptMgr',
+    [Parameter(Mandatory = $true, ParameterSetName='Credential')]
+    [pscredential]$GitUserCredential,
+    [Parameter(Mandatory = $true, ParameterSetName='UserName')]
+    [string]$GitUserName, 
+    [string]$SRLibraryPath = 'C:\ProgramData\AppSphere\ScriptMgr\Git',
     [ValidateSet('clone','pull')]
     [string]$GitAction = 'pull'
 )
 
+if(-not (Test-Path -Path $SRLibraryPath -ErrorAction SilentlyContinue)){
+    New-Item -Path $SRLibraryPath -ItemType 'Directory' -Force
+}
+
 if($GitRepoUrl.Trim().StartsWith('https://') -or $GitRepoUrl.Trim().StartsWith('http://')){
     $i = $GitRepoUrl.IndexOf('://')
     $i += 3
-    $GitRepoUrl = $GitRepoUrl.Insert($i, $GitUser.UserName + "@")
-    Write-Output "$GitAction $GitRepoUrl ..."
+    if($PSCmdlet.ParameterSetName -eq 'Credential'){
+        $cred = New-Object -TypeName 'System.Net.NetworkCredential' -ArgumentList @($GitUserCredential.UserName, $GitUserCredential.Password)
+        $gitUrl = $GitRepoUrl.Insert($i, $cred.UserName + ':' + $([uri]::EscapeDataString($cred.Password)) + '@')
+        Write-Output "$GitAction $($gitUrl.Replace($([uri]::EscapeDataString($cred.Password)), '*****')) ..."
+    }
+    if($PSCmdlet.ParameterSetName -eq 'UserName'){
+        $gitUrl = $GitRepoUrl.Insert($i, $GitUserName + '@')
+        Write-Output "$GitAction $gitUrl ..."
+    }
 }
 else {
-    throw "Invalid git URL '$GitRepoUrl'."
+    Write-Error -Message "Invalid git URL '$GitRepoUrl'." -ErrorAction 'Stop'
 }
 
 if(Test-Path -Path $SRLibraryPath -ErrorAction SilentlyContinue){
     $currentLocation = Get-Location
 
     if($GitAction -eq 'pull'){
-        $i = $GitRepoUrl.LastIndexOf('/')
+        $i = $gitUrl.LastIndexOf('/')
         $i++
-        $repo = $GitRepoUrl.Substring($i)
+        $repo = $gitUrl.Substring($i)
         $repo = $repo.Split('.')[0]
         Write-Output "Repository: '$repo'."
         $SRLibraryPath = Join-Path -Path $SRLibraryPath -ChildPath $repo
         Write-Output "Local repository path: '$SRLibraryPath'."
     }
 
-    Set-Location -Path $SRLibraryPath -PassThru
+    Set-Location -Path $SRLibraryPath
     try {
-        & git @($GitAction, $GitRepoUrl)
-        
+        $resultMessage = & 'git.exe' @($GitAction, $gitUrl)
+        $resultMessage
     }
     catch {
-        $_.CategoryInfo
+        $_
     }
     finally{
-        $currentLocation | Set-Location -PassThru
+        $currentLocation | Set-Location
+        if($SRXEnv){
+            $SRXEnv.ResultMessage = $resultMessage
+        }
         if ($LASTEXITCODE -ne 0) {
-            $Error[0]
-            throw "Error occured: '$($Error[0].FullyQualifiedErrorId) $($Error[0].ScriptStackTrace)'."
+            $err = $Error[0]
+            $err
+            if($SRXEnv){
+                $SRXEnv.ResultMessage += $err.Exception
+            }
+            Write-Error -Message "Failed to $GitAction $GitRepoUrl" -ErrorAction 'Stop'
         }
     }
-
 }
 else {
-    throw "ScriptRunner Library Path '$SRLibraryPath' does not exist."
+    Write-Error -Message "ScriptRunner Library Path '$SRLibraryPath' does not exist." -ErrorAction 'Stop'
 }

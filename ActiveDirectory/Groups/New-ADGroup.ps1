6856383bac5c2ba9c7110f25c51d51759eb11d56
haraldpfirmann <harald.pfirmann@appsphere.com>
2017-07-12 10:03:04 +0200

    Notes assigned

--- ActiveDirectory/Groups/New-ADGroup.ps1
+++ ActiveDirectory/Groups/New-ADGroup.ps1
@@ -4,8 +4,16 @@
     .SYNOPSIS
         Creates a group in the OU path
     
-    .DESCRIPTION         
-    
+    .DESCRIPTION  
+
+    .NOTES
+        This PowerShell script was developed and optimized for ScriptRunner. The use of the scripts requires ScriptRunner. 
+        The customer or user is authorized to copy the script from the repository and use them in ScriptRunner. 
+        The terms of use for ScriptRunner do not apply to this script. In particular, AppSphere AG assumes no liability for the function, 
+        the use and the consequences of the use of this freely available script.
+        PowerShell is a product of Microsoft Corporation. ScriptRunner is a product of AppSphere AG.
+        © AppSphere AG
+
     .Parameter GroupName
         Specifies the name of the new group
     

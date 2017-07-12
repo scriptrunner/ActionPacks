21ba56d22ebedcfb42497bd7a691eacd89d3e7bc
haraldpfirmann <harald.pfirmann@appsphere.com>
2017-07-12 10:39:12 +0200

    Notes assigned

--- O365/MSOnline/Users/Set-MsOUserProperties.ps1
+++ O365/MSOnline/Users/Set-MsOUserProperties.ps1
@@ -9,7 +9,15 @@
         Microsoft Online Sign-In Assistant for IT Professionals  
         Azure Active Diretory Powershell Module v1
     
-    .DESCRIPTION          
+    .DESCRIPTION  
+
+    .NOTES
+        This PowerShell script was developed and optimized for ScriptRunner. The use of the scripts requires ScriptRunner. 
+        The customer or user is authorized to copy the script from the repository and use them in ScriptRunner. 
+        The terms of use for ScriptRunner do not apply to this script. In particular, AppSphere AG assumes no liability for the function, 
+        the use and the consequences of the use of this freely available script.
+        PowerShell is a product of Microsoft Corporation. ScriptRunner is a product of AppSphere AG.
+        © AppSphere AG
 
     .Parameter O365Account
         Specifies the credential to use to connect to Azure Active Directory

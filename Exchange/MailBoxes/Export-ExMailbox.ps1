0306398a62da769fb0a2eea12ea259d86cae7f94
haraldpfirmann <harald.pfirmann@appspere.com>
2018-01-03 11:21:34 +0100

    New scripts

---
+++ Exchange/MailBoxes/Export-ExMailbox.ps1
@@ -0,0 +1,62 @@
+﻿#Requires -Version 4.0
+
+<#
+    .SYNOPSIS
+        Exporting contents of a primary mailbox or archive to a .pst file
+    
+    .DESCRIPTION  
+
+    .NOTES
+        This PowerShell script was developed and optimized for ScriptRunner. The use of the scripts requires ScriptRunner. 
+        The customer or user is authorized to copy the script from the repository and use them in ScriptRunner. 
+        The terms of use for ScriptRunner do not apply to this script. In particular, AppSphere AG assumes no liability for the function, 
+        the use and the consequences of the use of this freely available script.
+        PowerShell is a product of Microsoft Corporation. ScriptRunner is a product of AppSphere AG.
+        © AppSphere AG
+
+    .COMPONENT       
+        On-premises Exchange Server 2016
+        ScriptRunner Version 4.x or higher
+
+    .LINK
+        https://github.com/scriptrunner/ActionPacks/tree/master/Exchange/MailBoxes
+
+    .Parameter MailboxId
+        Specifies the Alias, Display name, Distinguished name, SamAccountName, Guid or user principal name of the mailbox
+
+    .Parameter FilePath 
+        The FilePath parameter specifies the network share path of the .pst file to which data is exported
+
+    .Parameter IsArchiveBox 
+        Specifies that you're exporting from the user's archive
+#>
+
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$MailboxId,
+    [Parameter(Mandatory = $true)]
+    [string]$FilePath,
+    [switch]$IsArchiveBox
+)
+
+#Clear
+    try{
+        $res = Get-Mailbox -Identity $MailboxId 
+        if($null -ne $res){        
+            New-MailboxExportRequest -Mailbox $MailboxId -FilePath $FilePath -IsArchive:$IsArchiveBox -ErrorAction Stop
+            if($SRXEnv) {
+                $SRXEnv.ResultMessage = "Mailbox $($MailboxId) exported"
+            }
+            else{
+                Write-Output "Mailbox $($MailboxId) exported"
+            }
+        }
+        else{
+            if($SRXEnv) {
+                $SRXEnv.ResultMessage = "Mailbox $($MailboxId) not found"
+            } 
+            Throw  "Mailbox $($MailboxId) not found"
+        }
+    }
+    Finally{
+    }
---
+++ Exchange/MailBoxes/Set-ExMailboxForward.ps1
@@ -0,0 +1,66 @@
+﻿#Requires -Version 4.0
+
+<#
+    .SYNOPSIS
+        Connect to Microsoft Exchange Server and forwards mail from one mailbox to another mailbox
+    
+    .DESCRIPTION  
+
+    .NOTES
+        This PowerShell script was developed and optimized for ScriptRunner. The use of the scripts requires ScriptRunner. 
+        The customer or user is authorized to copy the script from the repository and use them in ScriptRunner. 
+        The terms of use for ScriptRunner do not apply to this script. In particular, AppSphere AG assumes no liability for the function, 
+        the use and the consequences of the use of this freely available script.
+        PowerShell is a product of Microsoft Corporation. ScriptRunner is a product of AppSphere AG.
+        © AppSphere AG
+
+    .COMPONENT       
+        On-premises Exchange Server 2016
+        ScriptRunner Version 4.x or higher
+
+    .LINK
+        https://github.com/scriptrunner/ActionPacks/tree/master/Exchange/MailBoxes
+
+    .Parameter MailboxId
+        Specifies the Alias, Display name, Distinguished name, SamAccountName, Guid or user principal name of the mailbox
+
+    .Parameter ForwardTo
+        Specifies the Alias, Display name, Distinguished name, SamAccountName, Guid or user principal name of a recipient to forward the message to
+
+    .Parameter RuleName 
+        Specifies a name for the Inbox rule being created
+#>
+
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$MailboxId,
+    [Parameter(Mandatory = $true)]
+    [string]$ForwardTo,
+    [string]$RuleName
+)
+
+#Clear
+    try{
+        $res = Get-Mailbox -Identity $MailboxId | Select-Object Name
+        $forto = Get-Mailbox -Identity $ForwardTo | Select-Object Name
+        if($null -ne $res -and $null -ne $forto){
+            if([System.String]::IsNullOrWhiteSpace($RuleName)){
+                $RuleName = "$($res.Name)to$($forto.Name)"
+            }
+            New-InboxRule -Name $RuleName -Mailbox $res.Name -ForwardTo $forto.Name -Force -ErrorAction Stop
+            if($SRXEnv) {
+                $SRXEnv.ResultMessage = "Mailbox $($MailboxId) forward to $($ForwardTo)"
+            }
+            else{
+                Write-Output "Mailbox $($MailboxId) forward to $($ForwardTo)"
+            }
+        }
+        else{
+            if($SRXEnv) {
+                $SRXEnv.ResultMessage = "Mailbox $($MailboxId) or $($ForwardTo) not found"
+            } 
+            Throw  "Mailbox $($MailboxId) or $($ForwardTo) not found"
+        }
+    }
+    Finally{
+    }

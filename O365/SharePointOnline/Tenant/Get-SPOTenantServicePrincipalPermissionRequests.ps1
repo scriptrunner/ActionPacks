da1f5890f28f842b9831933d18f2da936b847f74
haraldpfirmann <harald.pfirmann@appspere.com>
2019-10-01 14:19:45 +0200

    Update notes

--- O365/SharePointOnline/Tenant/Get-SPOTenantServicePrincipalPermissionRequests.ps1
+++ O365/SharePointOnline/Tenant/Get-SPOTenantServicePrincipalPermissionRequests.ps1
@@ -10,10 +10,10 @@
     .NOTES
         This PowerShell script was developed and optimized for ScriptRunner. The use of the scripts requires ScriptRunner. 
         The customer or user is authorized to copy the script from the repository and use them in ScriptRunner. 
-        The terms of use for ScriptRunner do not apply to this script. In particular, AppSphere AG assumes no liability for the function, 
+        The terms of use for ScriptRunner do not apply to this script. In particular, ScriptRunner Software GmbH assumes no liability for the function, 
         the use and the consequences of the use of this freely available script.
-        PowerShell is a product of Microsoft Corporation. ScriptRunner is a product of AppSphere AG.
-        © AppSphere AG
+        PowerShell is a product of Microsoft Corporation. ScriptRunner is a product of ScriptRunner Software GmbH.
+        © ScriptRunner Software GmbH
 
     .COMPONENT
         Requires Module Microsoft.Online.SharePoint.PowerShell

034f64af9186b4b18544ce904279f1be5509e90e
haraldpfirmann <harald.pfirmann@scriptrunner.com>
2020-10-28 14:32:10 +0100

    Fix

--- ActiveDirectory/Users/Get-ADUserProperties.ps1
+++ ActiveDirectory/Users/Get-ADUserProperties.ps1
@@ -61,7 +61,7 @@ param(
     [Parameter(ParameterSetName = "Local or Remote DC")]
     [Parameter(ParameterSetName = "Remote Jumphost")]
     [ValidateSet('*','GivenName','Surname','DisplayName','DistinguishedName','Description','Enabled','Office','EmailAddress','OfficePhone','Title','Department','Company','StreetAddress','PostalCode','City','SAMAccountName','UserPrincipalName','MemberOf','LastLogonDate','LastBadPasswordAttempt','AccountExpirationDate','CanonicalName')]
-    [string]$Properties = @('Name','GivenName','Surname','DisplayName','Description','Office','EmailAddress','OfficePhone','Title','Department','Company','StreetAddress','PostalCode','City','SAMAccountName'),
+    [string[]]$Properties = @('Name','GivenName','Surname','DisplayName','Description','Office','EmailAddress','OfficePhone','Title','Department','Company','StreetAddress','PostalCode','City','SAMAccountName'),
     [Parameter(ParameterSetName = "Local or Remote DC")]
     [Parameter(ParameterSetName = "Remote Jumphost")]
     [string]$DomainName,

8b72afa6e4fd5211154a0ae6c8eb50f71283c4f9
AchimWieser <achim.wieser@scriptrunner.com>
2021-06-21 10:46:42 +0200

    Add webservice connector sample scripts.

---
+++ ScriptRunner/WebSvcConnector/CallASRWebSvcConnector2.ps1
@@ -0,0 +1,152 @@
+﻿<#
+	.SYNOPSIS
+	Start an action with parameters using the WebServiceConnector API2.
+
+	.PARAMETER ActionName
+	The name (exact name string) of the action to execute.
+
+	.PARAMETER ParamNames
+	Optional: List of parameter names required for the action (e.g., "abra","bebra")
+	(i.e., the script parameters you want to set).
+
+	.PARAMETER ParamValues
+	Optional: List of parameter values (matching the ParamNames list; e.g., "val1","val2")
+
+	.PARAMETER StartedBy
+	Optional: Name or email of the end user to run this action for.
+	If this parameter is specified, this name will show in the report as Started By,
+	and will be set to $SRXEnv.SRXStartedBy for your script. Otherwise the
+	WebServiceConnector service account will be used.
+
+	.PARAMETER TargetName
+	Optional: Target override - the name (exact displayname string) of the target to use.
+	A target override is possible only if the action has no targets configured,
+	or if the action requires a selection from a list of given possible targets.
+
+	.PARAMETER Reason
+	Optional: String describing the root cause why the action was executed,
+	for the report.
+#>
+
+Param
+(
+	[Parameter(Mandatory=$true)]
+	[string]$ActionName,
+	[string[]]$ParamNames,
+	[string[]]$ParamValues,
+	[string]$StartedBy = '',
+	[string]$TargetName,
+	[string]$Reason = ''
+)
+
+function Get-ASRJobControl
+{
+	PARAM(
+		[string]$server,
+		[int]$id,
+		[ref]$jc
+	)
+	Get-ASRJobControlFromUri -Uri "http://$server/ScriptRunner/JobControl($id)" -jc ([ref]$jc)
+}
+
+function Get-ASRJobControlFromUri
+{
+	PARAM(
+		[string]$Uri,
+		[ref]$jc
+	)
+	$json = $null
+	$json = Invoke-WebRequest -Uri $Uri -Method Get -UseDefaultCredentials -ContentType application/json -UseBasicParsing
+	if (!$json) {
+		throw 'Request failed.'
+	}
+	$list = $json.Content | ConvertFrom-Json
+	$jc.Value = $list;
+}
+
+# set your ScriptRunner server here; for local loopback connector simply set 'localhost:port_number'
+$server='localhost:8091'
+
+"Starting action $ActionName ..."
+# commands to start the action with the specified value
+$uri = "http://$server/ScriptRunner/api2/StartAction"
+
+# Create a $bodyobj object to build up the JSON body for the web request, e.g. something like this:
+####$body = '{"ActionName": "My Remote Action","sr_ValueA":15,"sr_ValueB":5, "StartedBy": "user@azure.org", "Reason":"Why do we do this"}'
+
+$bodyobj = @{}
+$bodyobj['ActionName'] = $ActionName
+if ($ParamNames) {
+	for ($i=0; $i -lt $ParamNames.Length; $i++) {
+		$pname = $ParamNames[$i]
+		$pvalue = $ParamValues[$i]
+		$bodyobj['sr_' + $pname] = $pvalue
+	}
+}
+if ($StartedBy) { $bodyobj['StartedBy'] = $StartedBy }
+if ($TargetName) { $bodyobj['TargetName'] = $TargetName }
+if ($Reason) { $bodyobj['Reason'] = $Reason }
+$bodyobj['WaitTime'] = 0
+
+$body = ''
+$body = $bodyobj | ConvertTo-Json
+$body
+
+# This is the web request (POST with JSON body data as defined above).
+# Sending the request with e.g. CURL.EXE would be very similar!
+$json = $null
+$json = Invoke-WebRequest -Uri $uri -Body $body -Method Post -UseDefaultCredentials -ContentType 'application/json; charset=utf-8' -UseBasicParsing
+if (!$json) {
+	throw 'Request failed.'
+}
+
+# show the response
+'' + $json.RawContentLength + ' Bytes: ' + $json.StatusCode + ' ' + $json.StatusDescription
+'...'
+if ($json.StatusCode -eq 202) {
+	$json.Headers
+	$Uri = $json.Headers['Location']
+	"Relocating to $Uri ..."
+}
+elseif ($json.StatusCode -eq 200) {
+	$list = $json.Content | ConvertFrom-Json
+	# This is the ID of the JobControl instance containing the result
+	$jcids = $list.value.ID
+	$id = $jcids[0]
+	# in case of multiple targets, $jcids is a list.
+	"Report ID: $jcids (Count = " + $jcids.Count + ")"
+	$Uri = "http://$server/ScriptRunner/JobControl($id)"
+}
+else {
+	throw "Error: Status Code $($json.StatusCode)"
+}
+# In case of $list.value.Running=True, you would have to poll each JobControl($jcid) for the script to finish:
+# GET to URI "http://$server/ScriptRunner/JobControl($jcid)", as in
+# Invoke-WebRequest -Uri "http://$server/ScriptRunner/JobControl($jcid)" -Method Get -UseDefaultCredentials -ContentType application/json -UseBasicParsing
+'...'
+Start-Sleep -Seconds 2
+$running = $true
+while ($running)
+{
+	$running = $false
+	$jc = $null
+	Get-ASRJobControlFromUri -Uri $Uri -jc ([ref]$jc)
+	$running = $running -OR $jc.Running
+	if ($running) {
+		'running...'
+		Start-Sleep -Seconds 1
+	}
+}
+
+# if the job has finished, we can access the result report.
+# this is the PowerShell report, in case $jc.Running=False
+if ($jc.OutReportString) {
+	"Report($Uri):"
+	'================================='
+	$jc.OutReportString
+	'================================='
+}
+# This is the result message, after $jc.Running=False.
+if ($jc.OutResultMessage) {
+	"ResultMessage: '" + $jc.OutResultMessage + "'"
+}
---
+++ ScriptRunner/WebSvcConnector/CallASRWebhook.ps1
@@ -0,0 +1,127 @@
+﻿<#
+    .SYNOPSIS
+    Trigger a ScriptRunner Action using the Webhook interface.
+
+    .DESCRIPTION
+    Trigger a ScriptRunner Action using the Webhook interface, with a POST request to the
+    ScriptRunner Webhook URI of this Action.
+    The Action and ScriptRunner endpoint are given either as the ScriptRunner endpoint
+    (like 'http://server:port/ScriptRunner/') and Action ID, or as the complete Action Webhook endpoint URI.
+
+    .PARAMETER bodyString
+    Arbitrary payload string to be transfered in the body of the POST request.
+    To send structured data, like a Hashtable, send it as a JSON string, and decode it in your script
+    using ConvertFrom-Json.
+
+    .PARAMETER actionID
+    The ID of the ScriptRunner Action to execute.
+    You find the ID of an Action in the Admin App, on the first card of the Action Edit Wizard.
+
+    .PARAMETER endpoint
+    The ScriptRunner endpoint for the WebService connector, like 'http://server:port/ScriptRunner/' or
+    'https://server:port/ScriptRunner/'. The default ScriptRunner port is 8091; the ScriptRunner Apps
+    show the UI endpoint they use in the About dialog.
+    Note that a different IP port may apply for the WebService Connector, depending on your
+    authentication settings (STS endpoint, Basic Auth,...).
+
+    .PARAMETER actionUri
+    The complete Webhook URI of the action to execute.
+    The Webhook URI for an Action is something like "http://server:port/ScriptRunner/api2/PostWebhook/ID",
+    with the proper ScriptRunner endpoint for your WebService Connector (http/https, server, port) 
+    and the ID of the Action.
+    You find the Webhook URI of an Action in the Admin App, on the first card of the Action Edit Wizard.
+
+    .PARAMETER basicAuthCreds
+    Optional: Basic Auth credentials, to use Basic Authentication (on the ScriptRunner STS port).
+    Without this parameter, the call will use Windows Integrated Auth for the current user
+    (Invoke-WebRequest -UseDefaultCredentials).
+#>
+
+[CmdletBinding()]
+
+Param
+(
+    [Parameter(Position=0)]
+    [string]$bodyString = '',
+    [Parameter(Mandatory=$true, ParameterSetName="Uri")]
+    [string]$actionUri,
+    [Parameter(Mandatory=$true, ParameterSetName="ID")]
+    [string]$actionID,
+    [Parameter(ParameterSetName="ID")]
+    [string]$endpoint,
+    [pscredential]$basicAuthCreds = $null
+)
+
+# You can set your ScriptRunner server here, and run the script with the Action ID, without endpoint parameter.
+# For local loopback connector on the ScriptRunner host, simply use 'localhost:port_number'.
+# The resulting Webhook URI to start the Action will be something like
+#$uri = "http://$defaultserver/ScriptRunner/api2/PostWebhook/$actionID"
+$defaultserver = 'localhost:8091'
+$defaultendpoint = "http://$defaultserver/ScriptRunner/"
+
+$uri = ''
+if ($PSCmdlet.ParameterSetName -eq 'Uri') {
+    $uri = $actionUri
+}
+else {
+    if (!$endpoint) { $endpoint = $defaultendpoint }
+    if (!$endpoint.EndsWith('/')) { $endpoint += '/' }
+    if (!$endpoint.EndsWith('ScriptRunner/')) { $endpoint += 'ScriptRunner/' }
+    $uri = $endpoint + "api2/PostWebhook/$actionID"
+}
+
+# JSON body example:
+#$bodyString = '{ "data": "This is JSON" }'
+# ...or construct JSON from a Hashtable:
+#$bodyobj = @{}
+#$bodyobj['data'] = 'This is JSON'
+#$bodyString = $bodyobj | ConvertTo-Json
+
+"Starting action $actionID with $($bodyString.Length) chars of data..."
+"   -> POST $uri"
+# This is the web request (POST with JSON body data as defined above).
+# Sending the request with e.g. CURL.EXE would be very similar!
+$json = $null
+if ($basicAuthCreds) {
+    # Basic Auth, on ScriptRunner STS port
+    $basicAuthValue = 'Basic ' + [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$($basicAuthCreds.UserName):$($basicAuthCreds.GetNetworkCredential().Password)"))
+    $Headers = @{ Authorization = $basicAuthValue }
+    $json = Invoke-WebRequest -Uri $uri -Body $bodyString -Method Post -Headers $Headers -ContentType 'application/json; charset=utf-8' -UseBasicParsing
+}
+else {
+    # Windows Integrated Auth
+    $json = Invoke-WebRequest -Uri $uri -Body $bodyString -Method Post -UseDefaultCredentials -ContentType 'application/json; charset=utf-8' -UseBasicParsing
+}
+#$json
+if (!$json) {
+    throw 'Request failed.'
+}
+
+# show the response
+'   <- ' + $json.RawContentLength + ' Bytes: ' + $json.StatusCode + ' ' + $json.StatusDescription
+if (($json.StatusCode -ge 200) -and ($json.StatusCode -lt 400)) {
+    '...Action triggered successfully!'
+    ''
+    #'Response headers:'
+    #$json.Headers | Format-Table
+    $res = $json.Headers['Location']
+    if ($res) {
+        "See $res for the result."
+        # The Location header may, depending on the version of the ScriptRunner host, contain an URI like
+        # "http://server:port/ScriptRunner/JobControl(ID)", with the ID of the result.
+        # Poll this URI until the returned JobControl structure has Running=False, at which point the script has finished.
+        # Then the JobControl structure will contain the results along with the script execution report:
+        #$json = $null
+        #if ($basicAuthCreds) {
+        #    $json = Invoke-WebRequest -Uri $res -Method Get -Headers $Headers -ContentType application/json -UseBasicParsing
+        #} else {
+        #    $json = Invoke-WebRequest -Uri $res -Method Get -UseDefaultCredentials -ContentType application/json -UseBasicParsing
+        #}
+        #if ($json) { $jc = $json.Content | ConvertFrom-Json }
+        #$jc
+    }
+} 
+else {
+    $json
+}
+

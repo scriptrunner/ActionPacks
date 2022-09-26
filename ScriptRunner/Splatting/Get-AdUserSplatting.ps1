6eeac5fd31bb2cc19991653e3880250fb580a43d
Markus Hipp <markus.hipp@scriptrunner.com>
2022-09-26 14:56:39 +0200

    splatting example for ad users

---
+++ ScriptRunner/Splatting/Get-AdUserSplatting.ps1
@@ -0,0 +1,54 @@
+﻿<#
+.Synopsis
+.Description
+.Notes
+.Component
+.Parameter sAMAccountName
+    Nimmt den sAMAccountName wenn zu Testzwecken nach einem bestimmten Benutzer gesucht werden soll
+#>
+
+
+param (
+    #[Parameter(Mandatory = $true)]
+    #[string]$sAMAccountName,    
+    [string[]]$Properties = @("cn", "GivenName", "sn", "PostalCode", "sAMAccountName")
+)
+
+Import-Module ActiveDirectory
+
+try {   
+    $ADUsers = Get-ADUser -Filter '*' -Properties $Properties
+    foreach ($user in $ADUsers) {
+        [hashtable]$result = @{} #Hashtable zur Verwendung mit Splatting in der ScriptRunner Action
+        foreach ($prop in $Properties) {
+            $null = $result.Add($prop, $user.Item($prop).Value)            
+        }
+        if ($SRXEnv) {
+            $null = $SRXEnv.ResultList.Add($result)
+            $null = $SRXEnv.ResultList2.Add("$($user.cn)")        
+        }
+        else {
+            Write-Host "$($result)"
+        }   
+    }
+}
+catch {
+    throw
+}
+
+<#try {
+    $ADUser = Get-ADUser -Identity $sAMAccountName -Properties $Properties
+    [hashtable]$result = @{} #Hashtable zur Verwendung mit Splatting in der ScriptRunner Action
+    foreach ($prop in $Properties) {
+        $null = $result.Add($prop, $ADUser.Item($prop).Value)
+    }
+    if ($SRXEnv) {
+        $null = $SRXEnv.ResultList.Add($result)
+        $null = $SRXEnv.ResultList2.Add("$($ADUser.cn)")        
+    }
+}
+catch {
+    throw
+    
+}#>
+
---
+++ ScriptRunner/Splatting/Set-ADUserSplatting.ps1
@@ -0,0 +1,42 @@
+﻿<#
+.Synopsis
+.Description
+.Notes
+.Component
+.Parameter UserIdentity
+    Nimmt einen AD-Benutzer aus einer Query
+.Parameter PostalCode
+    zeigt die PLZ eines AD-Benutzers
+.Parameter GivenName
+    zeigt den Vornamen eines AD-Benutzers
+.Parameter sn
+    zeigt den Nachnamen eines AD-Benutzers
+.Parameter streetAddress
+    zeigt die Straße eines AD-Benutzers
+#>
+
+
+param (
+    [Parameter(Mandatory = $true, HelpMessage = "ASRDisplay(Splatting)")] #Notwendig damit die Attribute der $UserIdentity im Script verwendet werden können 
+    [hashtable]$UserIdentity,
+    [string]$PostalCode,
+    [string]$GivenName,
+    [string]$sn,
+    [pscredential]$cred    
+)
+
+Import-Module ActiveDirectory
+
+try {
+    [hashtable]$Properties = @{
+        'Identity' = $UserIdentity.sAMAccountName #Hier wird mittels Splatting der sAMAccountName aus dem Parameter (Hashtable) $UserIdentity verwendet
+        'PostalCode' = $PostalCode
+        'GivenName' = $GivenName
+        'sn' = $sn
+    }
+
+    Set-ADUser @Properties #Mittels Splatting werden die Attribute des AD Benutzers gesetzt
+}
+catch {
+    throw
+}

d4d695308183eecfbbb2e9db8c0d46e7bb8178de
haraldpfirmann <harald.pfirmann@scriptrunner.com>
2022-11-08 15:39:49 +0100

    Change Import-Module

--- VMware/Host/Set-VMHVHostDiagnosticPartition.ps1
+++ VMware/Host/Set-VMHVHostDiagnosticPartition.ps1
@@ -1,5 +1,5 @@
-#Requires -Version 4.0
-# Requires -Modules VMware.PowerCLI
+﻿#Requires -Version 5.0
+# Requires -Modules VMware.VimAutomation.Core
 
 <#
 .SYNOPSIS
@@ -16,7 +16,7 @@
     © ScriptRunner Software GmbH
 
 .COMPONENT
-    Requires Module VMware.PowerCLI
+    Requires Module VMware.VimAutomation.Core
 
 .LINK
     https://github.com/scriptrunner/ActionPacks/tree/master/VMware/Host
@@ -46,7 +46,7 @@ Param(
     [bool]$Active
 )
 
-Import-Module VMware.PowerCLI
+Import-Module VMware.VimAutomation.Core
 
 try{
     $Script:vmServer = Connect-VIServer -Server $VIServer -Credential $VICredential -ErrorAction Stop

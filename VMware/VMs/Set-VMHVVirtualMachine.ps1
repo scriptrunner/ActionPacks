a12954f17129e653bc267fba93d7803de93717b0
haraldpfirmann <harald.pfirmann@scriptrunner.com>
2023-04-12 12:12:16 +0200

    Splatting parameterset by set vm

--- VMware/VMs/Set-VMHVVirtualMachine.ps1
+++ VMware/VMs/Set-VMHVVirtualMachine.ps1
@@ -1,5 +1,5 @@
 ﻿#Requires -Version 5.0
-# Requires -Modules VMware.VimAutomation.Core
+#Requires -Modules VMware.VIMAutomation.Core
 
 <#
     .SYNOPSIS
@@ -16,26 +16,19 @@
         © ScriptRunner Software GmbH
 
     .COMPONENT
-        Requires Module VMware.VimAutomation.Core
-
-    .LINK
-        https://github.com/scriptrunner/ActionPacks/tree/master/VMware/VMs
+        Requires Module VMware.VIMAutomation.Core
 
     .Parameter VIServer
         [sr-en] IP address or the DNS name of the vSphere server to which you want to connect
-        [sr-de] IP-Adresse oder DNS des vSphere Servers
+        [sr-de] IP Adresse oder FQDN des VSphere Servers
 
     .Parameter VICredential
         [sr-en] PSCredential object that contains credentials for authenticating with the server
-        [sr-de] Anmeldedaten für die Authentifizierung beim Server
+        [sr-de] Benutzerkonto zur Authentifizierung am VSphere Server
 
-    .Parameter VMId
-        [sr-en] ID of the virtual machine
-        [sr-de] ID der VM
-
-    .Parameter VMName
-        [sr-en] Name of the virtual machine
-        [sr-de] Name der VM
+    .Parameter VM
+        [sr-en] Virtual machine you want to configure
+        [sr-de] VM
 
     .Parameter Notes
         [sr-en] Description for the virtual machine
@@ -43,23 +36,23 @@
 
     .Parameter Cpus
         [sr-en] Number of the virtual CPUs
-        [sr-de] Anzahl der Prozessoren
+        [sr-de] Anzahl der Cpus
 
     .Parameter CoresPerSocket
         [sr-en] Number of virtual CPU cores per socket
-        [sr-de] Anzahl der CPUs per Socket
+        [sr-de] Anzahl der Cpus pro Sockel
 
     .Parameter MemoryGB
         [sr-en] Memory size in gigabytes (GB)
-        [sr-de] Arbeitsspeicher in Gigabyte
+        [sr-de] Speicher in Gigabyte (GB)
 
     .Parameter Network 
         [sr-en] Network to which you want to connect the virtual machine
-        [sr-de] Netzwerk der VM
+        [sr-de] Netzwerk
 
     .Parameter GuestId
         [sr-en] Guest operating system
-        [sr-de] Betriebssystem der VM
+        [sr-de] Betriebssystem
 
     .Parameter OSCustomizationSpec
         [sr-en] Customization specification that is to be applied to the virtual machine. 
@@ -70,109 +63,167 @@
     .Parameter HardwareVersion
         [sr-en] Version to which you want to upgrade the virtual machine. 
         You cannot downgrade to an earlier version
-        [sr-de] Version auf die die VM aktualisiert wird
+        [sr-de] Neue Version der VM
 
     .Parameter VMSwapfilePolicy
         [sr-en] Swapfile placement policy
-        [sr-de] Regeln für die Auslagerungsdatei
+        [sr-de] Regel für die Auslagerungsdatei
 
     .Parameter NewName
         [sr-en] New name for the virtual machine
-        [sr-de] Neuer der Name der VM
+        [sr-de] Neuer Name der VM
+
+    .Parameter GuestDiskTotal
+        [sr-en] ReadOnly: Total size of the guest disk in gigabytes (GB)
+        [sr-de] ReadOnly: Größe der Betriebssystemfestplatte in Gigabytes (GB) 
+
+    .Parameter GuestDiskFree
+        [sr-en] ReadOnly: Free size on the guest disk in gigabytes (GB)
+        [sr-de] ReadOnly: Freier Platz auf der Betriebssystemfestplatte in Gigabytes (GB)         
+
+    .Parameter CpuUsageAverage
+        [sr-en] ReadOnly: Cpu usage average
+        [sr-de] ReadOnly: Durchschnittliche Cpu Auslastung 
+
+    .Parameter MemoryUsageAverage
+        [sr-en] ReadOnly: Memory usage average
+        [sr-de] ReadOnly: Durchschnittliche Speicher Auslastung 
 #>
 
 [CmdLetBinding()]
 Param(
     [Parameter(Mandatory = $true,ParameterSetName = "byID")]
     [Parameter(Mandatory = $true,ParameterSetName = "byName")]
+    [Parameter(Mandatory = $true,ParameterSetName = "Splatting")]
     [string]$VIServer,
     [Parameter(Mandatory = $true,ParameterSetName = "byID")]
     [Parameter(Mandatory = $true,ParameterSetName = "byName")]
+    [Parameter(Mandatory = $true,ParameterSetName = "Splatting")]
     [pscredential]$VICredential,
     [Parameter(Mandatory = $true,ParameterSetName = "byID")]
     [string]$VMId,
     [Parameter(Mandatory = $true,ParameterSetName = "byName")]
     [string]$VMName,
+    [Parameter(Mandatory = $true,ParameterSetName = "Splatting",HelpMessage="ASRDisplay(Splatting)")]
+    [hashtable]$VM,
     [Parameter(ParameterSetName = "byID")]
     [Parameter(ParameterSetName = "byName")]
+    [Parameter(ParameterSetName = "Splatting")]
     [string]$Notes,
     [Parameter(ParameterSetName = "byID")]
     [Parameter(ParameterSetName = "byName")]
+    [Parameter(ParameterSetName = "Splatting")]
     [int32]$Cpus,
     [Parameter(ParameterSetName = "byID")]
     [Parameter(ParameterSetName = "byName")]
+    [Parameter(ParameterSetName = "Splatting")]
     [int32]$CoresPerSocket,
     [Parameter(ParameterSetName = "byID")]
     [Parameter(ParameterSetName = "byName")]
+    [Parameter(ParameterSetName = "Splatting",HelpMessage="ASRDisplay(Alias=Memory)")]
     [decimal]$MemoryGB,
+    [Parameter(ParameterSetName = "Splatting",HelpMessage="ASRDisplay(ReadOnly)")]
+    [decimal]$CpuUsageAverage,
+    [Parameter(ParameterSetName = "Splatting",HelpMessage="ASRDisplay(ReadOnly)")]
+    [decimal]$MemoryUsageAverage,
+    [Parameter(ParameterSetName = "Splatting",HelpMessage="ASRDisplay(ReadOnly)")]
+    [decimal]$GuestDiskTotal,
+    [Parameter(ParameterSetName = "Splatting",HelpMessage="ASRDisplay(ReadOnly)")]
+    [decimal]$GuestDiskFree,
     [Parameter(ParameterSetName = "byID")]
     [Parameter(ParameterSetName = "byName")]
+    [Parameter(ParameterSetName = "Splatting")]
     [string]$Network,
     [Parameter(ParameterSetName = "byID")]
     [Parameter(ParameterSetName = "byName")]
+    [Parameter(ParameterSetName = "Splatting")]
     [string]$GuestId,
     [Parameter(ParameterSetName = "byID")]
     [Parameter(ParameterSetName = "byName")]
+    [Parameter(ParameterSetName = "Splatting")]
     [string]$OSCustomizationSpec,
     [Parameter(ParameterSetName = "byID")]
     [Parameter(ParameterSetName = "byName")]
+    [Parameter(ParameterSetName = "Splatting")]
     [string]$HardwareVersion,
     [Parameter(ParameterSetName = "byID")]
     [Parameter(ParameterSetName = "byName")]
+    [Parameter(ParameterSetName = "Splatting")]
     [ValidateSet("WithVM","Inherit","InHostDatastore")]
     [string]$VMSwapfilePolicy,
     [Parameter(ParameterSetName = "byID")]
     [Parameter(ParameterSetName = "byName")]
+    [Parameter(ParameterSetName = "Splatting")]
     [string]$NewName
 )
 
-Import-Module VMware.VimAutomation.Core
+Import-Module VMware.VIMAutomation.Core
 
 try{
     [string[]]$Properties = @('Name','Id','NumCpu','CoresPerSocket','Notes','GuestId','MemoryGB','VMSwapfilePolicy','ProvisionedSpaceGB','Folder')
-    $Script:vmServer = Connect-VIServer -Server $VIServer -Credential $VICredential -ErrorAction Stop
+    $vmServer = Connect-VIServer -Server $VIServer -Credential $VICredential -ErrorAction Stop
 
     if($PSCmdlet.ParameterSetName  -eq "byID"){
-        $Script:machine = Get-VM -Server $Script:vmServer -Id $VMId -ErrorAction Stop
+        $machine = Get-VM -Server $vmServer -Id $VMId -ErrorAction Stop
+    }
+    elseif($PSCmdlet.ParameterSetName -eq "Splatting"){
+        $machine = Get-VM -Server $vmServer -Id $VM.Id -ErrorAction Stop
     }
     else{
-        $Script:machine = Get-VM -Server $Script:vmServer -Name $VMName -ErrorAction Stop
+        $machine = Get-VM -Server $vmServer -Name $VMName -ErrorAction Stop
     }
 
-    if($Cpus -gt 0){
-        $null = Set-VM -Server $Script:vmServer -VM  $Script:machine -NumCpu $Cpus -Confirm:$False -ErrorAction Stop
+    if($PSCmdlet.ParameterSetName -eq "Splatting"){
+        if($Cpus -gt $machine.NumCpu){
+            $null = Set-VM -Server $vmServer -VM $machine -NumCpu $Cpus -Confirm:$False -ErrorAction Stop
+        }
+        if($CoresPerSocket -gt $machine.CoresPerSocket){
+            $null = Set-VM -Server $vmServer -VM $machine -CoresPerSocket $CoresPerSocket -Confirm:$False -ErrorAction Stop
+        }
+        if($MemoryGB -gt $machine.MemoryGB){
+            $null = Set-VM -Server $vmServer -VM $machine -MemoryGB $MemoryGB -Confirm:$False -ErrorAction Stop
+        }
+        if($Notes -cne $machine.Notes){
+            $null = Set-VM -Server $vmServer -VM $machine -Notes $Notes -Confirm:$False -ErrorAction Stop
+        }
     }
-    if($CoresPerSocket -gt 0){
-        $null = Set-VM -Server $Script:vmServer -VM  $Script:machine -CoresPerSocket $CoresPerSocket -Confirm:$False -ErrorAction Stop
+    else{
+        if($Cpus -gt 0){
+            $null = Set-VM -Server $vmServer -VM  $machine -NumCpu $Cpus -Confirm:$False -ErrorAction Stop
+        }
+        if($CoresPerSocket -gt 0){
+            $null = Set-VM -Server $vmServer -VM  $machine -CoresPerSocket $CoresPerSocket -Confirm:$False -ErrorAction Stop
+        }
+        if($MemoryGB -gt 0){
+            $null = Set-VM -Server $vmServer -VM  $machine -MemoryGB $MemoryGB -Confirm:$False -ErrorAction Stop
+        }
+        if($PSBoundParameters.ContainsKey('Notes') -eq $true){
+            $null = Set-VM -Server $vmServer -VM  $machine -Notes $Notes -Confirm:$False -ErrorAction Stop
+        }
     }
-    if($MemoryGB -gt 0){
-        $null = Set-VM -Server $Script:vmServer -VM  $Script:machine -MemoryGB $MemoryGB -Confirm:$False -ErrorAction Stop
-    }
-    if($PSBoundParameters.ContainsKey('Notes') -eq $true){
-        $null = Set-VM -Server $Script:vmServer -VM  $Script:machine -Notes $Notes -Confirm:$False -ErrorAction Stop
-    }
+
     if($PSBoundParameters.ContainsKey('GuestId') -eq $true){
-        $null = Set-VM -Server $Script:vmServer -VM  $Script:machine -GuestId $GuestId -Confirm:$False -ErrorAction Stop
+        $null = Set-VM -Server $vmServer -VM  $machine -GuestId $GuestId -Confirm:$False -ErrorAction Stop
     }
     if($PSBoundParameters.ContainsKey('OSCustomizationSpec') -eq $true){
-        $spec = Get-OSCustomizationSpec -Name $OSCustomizationSpec -Server $Script:vmServer
-        $null = Set-VM -Server $Script:vmServer -VM  $Script:machine -OSCustomizationSpec $spec -Confirm:$False -ErrorAction Stop
+        $spec = Get-OSCustomizationSpec -Name $OSCustomizationSpec -Server $vmServer
+        $null = Set-VM -Server $vmServer -VM  $machine -OSCustomizationSpec $spec -Confirm:$False -ErrorAction Stop
     }
     if($PSBoundParameters.ContainsKey('HardwareVersion') -eq $true){
-        $null = Set-VM -Server $Script:vmServer -VM  $Script:machine -HardwareVersion $HardwareVersion -Confirm:$False -ErrorAction Stop
+        $null = Set-VM -Server $vmServer -VM  $machine -HardwareVersion $HardwareVersion -Confirm:$False -ErrorAction Stop
     }
     if($PSBoundParameters.ContainsKey('Network') -eq $true){
-        $adapter = Get-NetworkAdapter -Server $Script:vmServer -VM $Script:machine
+        $adapter = Get-NetworkAdapter -Server $vmServer -VM $machine
         $null = Set-NetworkAdapter -NetworkName $Network -NetworkAdapter $adapter -Confirm:$false -ErrorAction Stop
     }
     if($PSBoundParameters.ContainsKey('VMSwapfilePolicy') -eq $true){
-        $null = Set-VM -Server $Script:vmServer -VM  $Script:machine -VMSwapfilePolicy $VMSwapfilePolicy -Confirm:$False -ErrorAction Stop
+        $null = Set-VM -Server $vmServer -VM  $machine -VMSwapfilePolicy $VMSwapfilePolicy -Confirm:$False -ErrorAction Stop
     }
     if($PSBoundParameters.ContainsKey('NewName') -eq $true){
-        $null = Set-VM -Server $Script:vmServer -VM  $Script:machine -Name $NewName -Confirm:$False -ErrorAction Stop
+        $null = Set-VM -Server $vmServer -VM  $machine -Name $NewName -Confirm:$False -ErrorAction Stop
     }
 
-    $result = Get-VM -Server $Script:vmServer -ID $Script:machine.Id | Select-Object $Properties
+    $result = Get-VM -Server $vmServer -ID $machine.Id | Select-Object $Properties
     if($SRXEnv) {
         $SRXEnv.ResultMessage = $result
     }
@@ -184,7 +235,7 @@ catch{
     throw
 }
 finally{    
-    if($null -ne $Script:vmServer){
-        Disconnect-VIServer -Server $Script:vmServer -Force -Confirm:$false
+    if($null -ne $vmServer){
+        Disconnect-VIServer -Server $vmServer -Force -Confirm:$false
     }
 }
---
+++ VMware/_QUERY_/QUY_Get-VMHVVirtualMachineIDSplatting.ps1
@@ -0,0 +1,75 @@
+﻿#Requires -Version 5.0
+#Requires -Modules VMware.VIMAutomation.Core
+
+<#
+.SYNOPSIS
+    Retrieves the properties of the virtual machines on a vCenter Server system
+
+.DESCRIPTION
+
+.NOTES
+    This PowerShell script was developed and optimized for ScriptRunner. The use of the scripts requires ScriptRunner. 
+    The customer or user is authorized to copy the script from the repository and use them in ScriptRunner. 
+    The terms of use for ScriptRunner do not apply to this script. In particular, ScriptRunner Software GmbH assumes no liability for the function, 
+    the use and the consequences of the use of this freely available script.
+    PowerShell is a product of Microsoft Corporation. ScriptRunner is a product of ScriptRunner Software GmbH.
+    © ScriptRunner Software GmbH
+
+.COMPONENT
+    Requires ModuleVMware.VIMAutomation.Core
+
+.LINK
+    https://github.com/scriptrunner/ActionPacks/tree/master/VMware/_QUERY_
+
+.Parameter VIServer
+    Specifies the IP address or the DNS name of the vSphere server to which you want to connect
+
+.Parameter VICredential
+    Specifies a PSCredential object that contains credentials for authenticating with the server
+#>
+
+[CmdLetBinding()]
+Param(
+    [Parameter(Mandatory = $true)]
+    [string]$VIServer,
+    [Parameter(Mandatory = $true)]
+    [pscredential]$VICredential
+)
+
+Import-Module VMware.VIMAutomation.Core
+
+try{    
+    $vmServer = Connect-VIServer -Server $VIServer -Credential $VICredential -ErrorAction Stop
+    $machines = Get-VM -Server $vmServer -ErrorAction Stop | Select-Object Id,Name,Notes,NumCpu,MemoryGB,CoresPerSocket | Sort-Object Name
+
+    foreach($item in $machines){
+        $disk = Get-VM -Server $vmServer -ErrorAction Stop -Id $item.Id | Get-VMGuestDisk
+        $stats = Get-VM -Server $vmServer -ErrorAction Stop -Id $item.Id | Get-Stat -Stat @('mem.usage.average','cpu.usage.average') -MaxSamples 1 -Start (Get-Date)
+        if($SRXEnv) {
+            $null = $SRXEnv.ResultList.Add(@{Id = $item.Id
+                                             Name = $item.Name   
+                                             Notes = $item.Notes
+                                             Cpus = $item.NumCpu
+                                             CoresPerSocket = $item.CoresPerSocket
+                                             Memory = $item.MemoryGB
+                                             GuestDiskTotal = [System.Math]::Round(($disk.CapacityGB | Measure-Object -Sum).Sum,3)
+                                             GuestDiskFree = [System.Math]::Round(($disk.FreeSpaceGB | Measure-Object -Sum).Sum,3)
+                                             'MemoryUsageAverage' = "$(($stats | Where-Object {$_.MetricID -eq 'mem.usage.average'}).Value) %"
+                                             'CpuUsageAverage' = "$(($stats | Where-Object {$_.MetricID -eq 'cpu.usage.average'}).Value) %"
+
+            })
+            $null = $SRXEnv.ResultList2.Add($item.Name) # Display
+        }
+        else{
+            Write-Output "$($item.Name) - $($item.Notes)"
+        }
+    }
+}
+catch{
+    throw
+}
+finally{    
+    if($null -ne $vmServer){
+        Disconnect-VIServer -Server $vmServer -Force -Confirm:$false
+    }
+}
